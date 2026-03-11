51a05f911e7ccd28f271cecdec18147fe5efec41
msiachild <164034381+msiachild@users.noreply.github.com>
2026-03-11 16:11:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,51 +4,16 @@ from datetime import date
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Google Sheets 连接
 scope = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_file(
-    "credentials.json",
-    scopes=scope
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope,
 )
 
 client = gspread.authorize(creds)
 
-# 用 Sheet ID 打开
 sheet = client.open_by_key("1rCd-REYtsmtQ48mLDYFcp-o_a5WVr8Ihqx9rWS3GDRE").sheet1
-
-st.title("📒 每月开销记录")
-
-d = st.date_input("日期", date.today())
-
-category = st.selectbox(
-    "类别",
-    ["餐饮","油费","购物","家庭","医疗","固定开销","其他"]
-)
-
-item = st.text_input("项目")
-
-amount = st.number_input("金额", min_value=0.0)
-
-if st.button("保存"):
-    sheet.append_row([str(d), category, item, amount])
-    st.success("记录成功")
-
-# 读取数据
-data = sheet.get_all_records()
-df = pd.DataFrame(data)
-
-st.subheader("记录")
-st.dataframe(df)
-
-st.subheader("总开销")
-if not df.empty:
-    st.write(df["amount"].sum())
-
-st.subheader("类别开销汇总")
-if not df.empty:
-    category_summary = df.groupby("category")["amount"].sum()
-    st.dataframe(category_summary)
